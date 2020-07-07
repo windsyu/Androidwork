9c062de095c5a0b158396e67880025d84b54d890
Windsyu <2738064693@qq.com>
2020-07-07 14:04:52 +0800

    Start my first try of Android development

---
+++ homework1/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button1);
+        final TextView tv = findViewById(R.id.text1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("It is too hard");
+            }
+        });
+
+        Log.i("00001","Click Button");
+    }
+
+
+}
